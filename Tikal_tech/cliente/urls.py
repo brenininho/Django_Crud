0be2a984247f67404703e25f4ad299ac127b0e4b
PC Roma <joaoricardoroma@gmail.com>
2021-04-24 13:11:46 -0300

    Registrando admin

--- Tikal_tech/cliente/urls.py
+++ Tikal_tech/cliente/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:cliente_id>/', views.detail, name='detail'),
+    path('<int:cliente_id>/results/', views.results, name='results'),
 ]
--- Tikal_tech/cliente/views.py
+++ Tikal_tech/cliente/views.py
@@ -2,4 +2,13 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world. You're at the client index.")
+
+
+def detail(request, cliente_id):
+    return HttpResponse("You're looking at client %s." % cliente_id)
+
+
+def results(request, cliente_id):
+    response = "You're looking at the results of client %s."
+    return HttpResponse(response % cliente_id)
