bcebb47d69ef864f14e9edf14352d292412a3544
PC Roma <joaoricardoroma@gmail.com>
2021-04-23 22:39:16 -0300

    corrigindo erro

--- Tikal_tech/cliente/models.py
+++ Tikal_tech/cliente/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+import uuid
+from dotenv import load_dotenv
+load_dotenv()
 
 
 class Client(models.Model):
@@ -14,8 +17,14 @@ class Client(models.Model):
     ])
 
 
+class Email(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    Client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    email = models.EmailField(max_length=254)
+
+
 class Telephone(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.AutoField(primary_key=True, default=uuid.uuid4, editable=False)
     Client = models.ForeignKey(Client, on_delete=models.CASCADE)
     ddd = models.IntegerField()
     number = models.IntegerField()
