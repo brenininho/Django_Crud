e914c87119cd2b0ed009e119c69ad174407a935e
PC Roma <joaoricardoroma@gmail.com>
2021-04-24 13:17:50 -0300

    Registrando admin

--- Tikal_tech/cliente/admin.py
+++ Tikal_tech/cliente/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Client
+from .models import Telephone
+from .models import Email
+
 
 admin.site.register(Client)
+admin.site.register(Telephone)
+admin.site.register(Email)
