64a99308be2257e93b35ffd343ab2b4bc62de9d4
Breno Valle <breno.valle9@hotmail.com>
2021-04-25 19:20:32 -0300

    modificando o arquivo views.

--- Tikal_tech/cliente/views.py
+++ Tikal_tech/cliente/views.py
@@ -1,6 +1,6 @@
 from django.views import generic
 from .models import Client
-from .forms import ClientForm
+from .forms import ClientForm, EmailForm, TelephoneForm
 from django.shortcuts import render
 
 
@@ -15,7 +15,24 @@ def delete(request):
 
 
 def cliente(request):
+    form_client = ClientForm()
+    form_email = EmailForm()
+    form_telephone = TelephoneForm
 
+    if request.method == 'POST':
+        form_client = ClientForm(request.POST)
+        form_email = EmailForm(request.POST)
+        form_telephone = TelephoneForm(request.POST)
+        if form_client == cliente:
+            pass
+        if form_email == email:
+            pass
+        if form_telephone == telephone:
+            pass
+        if form_client.is_valid():
+            form_client.save()
+
+    context = {'form': form_client}
     return render(request, 'cliente/cliente.html', context)
 
 
